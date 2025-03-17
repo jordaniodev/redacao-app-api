1a2dee74440c4a988b89b5c4309a7999a66a713e
jordaniodev <luigijordanio@gmail.com>
2025-03-17 09:39:51 -0300

    🔧 chore(scripts): update build and migration scripts
    - change build script to use migrate:production for production environment
    - add migrate:production script for production migrations
    
    🔧 chore(tsup): include knexFile.ts in build process
    
    - update tsup config to process knexFile.ts along with other TypeScript files

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/**/*.ts"], // Apenas arquivos TypeScript serão processados
+  entry: ["src/**/*.ts","knexFile.ts"], // Apenas arquivos TypeScript serão processados
   outDir: "build",
   format: ["cjs", "esm"], // Pode ajustar para o formato desejado
   dts: true, // Gera arquivos de definição de tipos (opcional)
