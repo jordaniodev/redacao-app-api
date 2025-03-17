cd80aba97faa9e0cf0f028ef4f3f4f0a24972775
jordaniodev <luigijordanio@gmail.com>
2025-03-17 11:00:16 -0300

    💄 style(package): correct file casing for knexfile path
    - adjust the casing of the knexfile path to ensure consistency and prevent potential file not found errors

--- tsup.config.ts
+++ tsup.config.ts
@@ -16,8 +16,5 @@ export default defineConfig([
     dts: false, // No type declarations needed for knexfile
     clean: false,
     skipNodeModulesBundle: true,
-    outExtension({ format }) {
-      return { js: format === "cjs" ? ".cjs" : ".js" }; // Forces .cjs output
-    },
   }
 ]);
