72c04620b47074edbd54b7d52584d00d52f15f4f
jordaniodev <luigijordanio@gmail.com>
2025-03-17 10:44:29 -0300

    🔧 chore(build): update knexfile extension and tsup config
    - change migrate:production script to use knexFile.cjs
    - modify tsup config to separate processing of knexFile.ts
    - ensure knexfile outputs as CommonJS with .cjs extension

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/**/*.ts","knexFile.ts"], // Apenas arquivos TypeScript serão processados
-  outDir: "build",
-  format: ["cjs", "esm"], // Pode ajustar para o formato desejado
-  dts: true, // Gera arquivos de definição de tipos (opcional)
-  clean: true, 
-  skipNodeModulesBundle: true,
-});
+export default defineConfig([
+  {
+    entry: ["src/**/*.ts"], // Process all TypeScript files except knexFile.ts
+    outDir: "build",
+    format: ["cjs", "esm"],
+    dts: true,
+    clean: true,
+    skipNodeModulesBundle: true,
+  },
+  {
+    entry: { "knexfile": "knexFile.ts" }, // Ensure this outputs knexfile.cjs
+    outDir: "build",
+    format: ["cjs"], // Force CommonJS
+    dts: false, // No type declarations needed for knexfile
+    clean: false,
+    skipNodeModulesBundle: true,
+    outExtension({ format }) {
+      return { js: format === "cjs" ? ".cjs" : ".js" }; // Forces .cjs output
+    },
+  }
+]);
