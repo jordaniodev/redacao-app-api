c9bbab5b4be2833dd8461c7b4466d871c9dfc84c
jordaniodev <luigijordanio@gmail.com>
2025-03-26 13:03:32 -0300

    Certificados

--- src/routes/webhook.routes.ts
+++ src/routes/webhook.routes.ts
@@ -4,5 +4,8 @@ export default async function (app: FastifyInstance) {
   app.post('/', async (req, res) => {
     return res.status(200).send();
   });
+  app.post('pix', async (req, res) => {
+    return res.status(200).send();
+  });
 }
   
