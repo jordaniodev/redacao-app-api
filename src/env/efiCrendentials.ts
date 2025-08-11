8fa9b557d4db367e89d5b3af2de9ba3f3b31811a
jennysol <jennyssoliver@gmail.com>
2025-08-11 18:01:47 -0300

    fix: sandbox false

--- src/env/efiCrendentials.ts
+++ src/env/efiCrendentials.ts
@@ -1,8 +1,8 @@
-import path from "path";
+// import path from "path";
 import { env } from ".";
 
 export default {
-    sandbox: true,
+    sandbox: false,
     client_id: env.EFI_BANK_API_CLIENT_ID,
     client_secret: env.EFI_BANK_API_CLIENT_SECRET,
     // certificate: env.EFI_BANK_API_ENV === `HMG` ? path.resolve(`certs/redacao-hmg.app.p12`) : path.resolve(`certs/redacao.app.p12`),
