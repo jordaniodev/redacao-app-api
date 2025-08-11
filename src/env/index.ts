11c26836b3c8c742cf6cee9206f8f023576dfd53
jennysol <jennyssoliver@gmail.com>
2025-08-11 20:15:36 -0300

    chore: send cert

--- src/env/index.ts
+++ src/env/index.ts
@@ -16,7 +16,8 @@ const envSchema = z.object({
     IA_GPT_TOKEN: z.string(),
     IA_DEEP_SEEK_TOKEN: z.string(),
     IA_DEFAULT_API: z.enum(['DEEP_SEEK', 'GPT']),
-    NODE_ENV: z.enum(['development', 'test', 'production']).default('development')
+    NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
+    EFI_CERT_BASE64: z.string(),
 })
 
 export const _env = envSchema.safeParse(process.env)
